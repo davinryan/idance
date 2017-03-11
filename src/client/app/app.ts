19a0172af016813bc3f9f7f2412902e425ce53cc
Davin Ryan <davin.ryan@solnet.co.nz>
2017-03-12 12:34:02 +1300

    added google analytics

--- src/client/app/app.ts
+++ src/client/app/app.ts
@@ -1,5 +1,5 @@
 import { Greeter } from "./greeter/greeter";
 
-const greeter: Greeter = new Greeter("Hello, world!");
+const greeter: Greeter = new Greeter("Hello Analytics Reporting API V4");
 
 document.getElementById("greeting").innerHTML = greeter.greet();
