6931dcc6e8d62d960e131d083550a22b13f688d0
Davin Ryan <davin.ryan@solnet.co.nz>
2017-03-12 14:42:09 +1300

    Created server backend

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 const path = require('path');
+const extend = require('extend');
+var webpack = require('webpack');
+var fs = require('fs');
 
-module.exports = {
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist/public')
-  },
+const config = {
   devtool: "source-map",
-  entry: {
-    app: ['./src/client/app/app.ts']
-  },
   module: {
     rules: [
       {
@@ -23,14 +19,47 @@ module.exports = {
       }
     ]
   },
+  resolve: {
+    extensions: ['.ts']
+  }
+};
+
+const clientConfig = extend(true, {}, config, {
+  target: 'web',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist/public')
+  },
+  entry: {
+    app: ['./src/client/app/app.ts']
+  },
   output: {
     path: path.resolve(__dirname, 'dist/public'),
     filename: 'app/bundle.js'
   },
   plugins: [
     new HtmlWebpackPlugin({template: './src/client/index.ejs'})
-  ],
-  resolve: {
-    extensions: ['.ts']
-  }
-};
+  ]
+});
+
+const nodeModules = {};
+fs.readdirSync('node_modules')
+    .filter(function (module) {
+      return ['.bin'].indexOf(module) === -1;
+    })
+    .forEach(function (module) {
+      nodeModules[module] = 'commonjs ' + module;
+    });
+
+const serverConfig = extend(true, {}, config, {
+  target: 'node',
+  entry: {
+    app: ['./src/server/index.ts']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/server'),
+    filename: 'index.js'
+  },
+  externals: nodeModules
+});
+
+module.exports = [clientConfig, serverConfig];
