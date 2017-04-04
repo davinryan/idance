4a24d9692b8558c667456152187245e3235238fc
Davin Ryan <davin.ryan@solnetsolutions.co.nz>
2017-04-04 21:02:46 +1200

    added better config tool

--- src/server/server.ts
+++ src/server/server.ts
@@ -57,8 +57,22 @@ class Server {
 
     // Analytics API
     router.get('/v1/reports', async(req, res) => {
-      let report = await this.reports.getNoEntrancesPerSiteSourceForLast30Days();
-      res.json(report)
+      if (req.query.type === 'noEntrancesPerSiteSourceForLast30Days') {
+        let report = await this.reports.getNoEntrancesPerSiteSourceForLast30Days();
+        res.json(report)
+      } else {
+        res.json({
+          usage: 'To use this service set a parameter called \'type\' to one of the following types',
+          types: [
+            {
+              type: 'noEntrancesPerSiteSourceForLast30Days',
+              description: 'Reports on the number of hits per site source used to enter the main site',
+              exampleUsage: '?type\=noEntrancesPerSiteSourceForLast30Days'
+            }
+          ]
+        });
+      }
+
     });
 
     // use router middleware
