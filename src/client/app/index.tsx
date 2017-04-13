2782f249bf02d628cfaef46063b2fe6ad163e9c7
Davin Ryan <davin.ryan@solnetsolutions.co.nz>
2017-04-14 08:45:57 +1200

    refactored routing

--- src/client/app/index.tsx
+++ src/client/app/index.tsx
@@ -1,24 +1,16 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom';
+import Routes from './routes/Routes';
 import {
   BrowserRouter as Router,
   browserHistory,
   Route,
   Link
 } from 'react-router-dom'
-import MainPage from './pages/MainPage';
 
 ReactDOM.render(
     <Router history={browserHistory}>
-      <div>
-        <ul>
-          <li><Link to="/analytics-reports/">Root</Link></li>
-          <li><Link to="/analytics-reports/mainPage">MainPage</Link></li>
-        </ul>
-
-        <Route exact path="/" component={MainPage}/>
-        <Route path="/analytics-reports/mainPage" component={MainPage}/>
-      </div>
+      <Routes />
     </Router>,
     document.getElementById('root')
 );
---
+++ src/client/app/routes/Routes.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react'
+import {
+  Route
+} from 'react-router-dom'
+import MainPage from '../pages/MainPage';
+
+interface IRoutes {}
+
+/**
+ * Default routes for this application.
+ */
+class Routes extends React.Component<IRoutes, any> {
+  render() {
+    return (
+        <Route path="/analytics-reports" component={MainPage}/>
+    );
+  }
+}
+
+export default Routes;
